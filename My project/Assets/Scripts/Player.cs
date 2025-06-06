19234a089557f3dec5c697a6e4e66112e20d93d1
Varsha Prasad <varshaprasad0601@gmail.com>
2025-06-06 19:38:38 -0400

    collider logic

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -3,6 +3,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public GameObject player;
+    public Item item;
+
+    private CircleCollider2D playerCircleCollider;
+    private BoxCollider2D itemBoxCollider;
+
+    void Start()
+    {
+        playerCircleCollider = player.GetComponent<CircleCollider2D>();
+        itemBoxCollider = item.GetComponent<BoxCollider2D>();
+    }
+
 
     void Update()
     {
@@ -11,5 +23,15 @@ public class PlayerMovement : MonoBehaviour
 
         Vector2 moveDir = new Vector2(moveX, moveY).normalized;
         transform.position += (Vector3)moveDir * moveSpeed * Time.deltaTime;
+
+        if (playerCircleCollider.IsTouching(itemBoxCollider))
+        {
+            Vector2 spawnPosition = new Vector2(3f, 2f);
+            Instantiate(item, spawnPosition, Quaternion.identity);
+
+        }
+
+
+
     }
 }
