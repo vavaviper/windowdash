a7aa07e6e19eb1b62eb0f2aeadc38d7295f99b9b
Sian <153153657+SianQ@users.noreply.github.com>
2025-06-06 20:21:38 -0400

    Update Item.cs

--- My project/Assets/Scripts/Item.cs
+++ My project/Assets/Scripts/Item.cs
@@ -7,39 +7,36 @@ public class Item : MonoBehaviour
     public string itemName = "Item";
     public Sprite inventoryIcon;
     
-    public Color highlightColor = Color.yellow;
     public float highlightIntensity = 1.5f;
-    public float highlightRadius = 1.5f; // How close player needs to be
+    public float highlightRadius = 1.5f;
     
     private SpriteRenderer spriteRenderer;
-    private Transform playerTransform;
+    private Color originalColor;
+    public float weight;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
         
         CircleCollider2D trigger = gameObject.AddComponent<CircleCollider2D>();
         trigger.radius = highlightRadius;
         trigger.isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void OnTriggerExit2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Highlight(false);
+            Highlight(true);
         }
     }
 
     void Highlight(bool enable)
     {
-        GetComponent<SpriteRenderer>().color = enable ? Color.yellow : Color.white;
+        spriteRenderer.color = enable ? 
+            Color.yellow * highlightIntensity : 
+            originalColor;
     }
-
 }
