c719a43eddc6e752853d957fbd52336867396f5b
AndiJingzhiTse <103150017+AndiJingzhiXie@users.noreply.github.com>
2025-06-07 15:32:19 -0400

    made garbage bin

---
+++ My project/Assets/Scripts/Garbage Bin.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class GarbageBin : MonoBehaviour
+{
+    private bool playerIsNear = false;
+    private Renderer binRenderer;
+    private Color originalColor;
+    public Color highlightColor = Color.yellow;
+
+    void Start()
+    {
+        binRenderer = GetComponent<Renderer>();
+        originalColor = binRenderer.material.color;
+    }
+
+    void Update()
+    {
+        if (playerIsNear && Input.GetKeyDown(KeyCode.F))
+        {
+            ClearInventory();
+        }
+    }
+
+    // Called by child trigger script
+    public void PlayerEntered()
+    {
+        playerIsNear = true;
+        Highlight(true);
+        Debug.Log("In range of garbage bin");
+    }
+
+    public void PlayerExited()
+    {
+        playerIsNear = false;
+        Highlight(false);
+        Debug.Log("Left range of garbage bin");
+    }
+
+    void Highlight(bool highlight)
+    {
+        binRenderer.material.color = highlight ? highlightColor : originalColor;
+    }
+
+    void ClearInventory()
+    {
+        Debug.Log("Inventory Cleared!");
+        // Your inventory clearing logic here
+        Inventory.instance.ClearInventory();
+    }
+}
